6b60a0fe6549ec0c7663056857850e77b2361f5e
Nemanja Popovic <nemanja23031991@yahoo.com>
2017-09-28 02:48:10 +0200

    Small styling updates

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-(function() {
+(function () {
     'use strict';
 
     var HOUSE_ID = 1;
+    var COLOR_UPDATE_TIMEOUT = 3000;
 
     // Initialize Firebase
     var config = {
@@ -17,26 +18,26 @@
     var database = firebase.database();
 
     var temperatureRef = database.ref(HOUSE_ID + '/temperature');
-    temperatureRef.on('value', function(snapshot) {
+    temperatureRef.on('value', function (snapshot) {
         updateTemperature(snapshot.val());
     });
 
     var humidityRef = database.ref(HOUSE_ID + '/humidity');
-    humidityRef.on('value', function(snapshot) {
+    humidityRef.on('value', function (snapshot) {
         updateHumidity(snapshot.val());
     });
 
     var opticalRef = database.ref(HOUSE_ID + '/optical');
-    opticalRef.on('value', function(snapshot) {
+    opticalRef.on('value', function (snapshot) {
         updateOptical(snapshot.val());
     });
 
     var pressureRef = database.ref(HOUSE_ID + '/pressure');
-    pressureRef.on('value', function(snapshot) {
+    pressureRef.on('value', function (snapshot) {
         updatePressure(snapshot.val());
     });
 
-    var sendMessage = function(type, value) {
+    var sendMessage = function (type, value) {
         database
             .ref(HOUSE_ID + '/' + type)
             .set({
@@ -45,44 +46,40 @@
             });
     };
 
-    var updateTemperature = function(value) {
-        console.log('New temperature is: ');
-        console.log(value);
-
+    var updateTemperature = function (value) {
         var temperature = value.value;
-        console.log(temperature);
-
         document.getElementById('temperature').textContent = temperature.toString() + ' ' + '°C';
+        document.getElementById('temperature-wrapper').classList.add('animate');
+        setTimeout(function () {
+            document.getElementById('temperature-wrapper').classList.remove('animate');
+        }, COLOR_UPDATE_TIMEOUT);
     };
 
-    var updateHumidity = function(value) {
-        console.log('New humidity is: ');
-        console.log(value);
-
+    var updateHumidity = function (value) {
         var humidity = value.value;
-        console.log(humidity);
-
-        document.getElementById('humidity').textContent = humidity.toString() + ' ' + value.unit;
+        document.getElementById('humidity').textContent = humidity.toString() + ' %RH';
+        document.getElementById('humidity-wrapper').classList.add('animate');
+        setTimeout(function () {
+            document.getElementById('humidity-wrapper').classList.remove('animate');
+        }, COLOR_UPDATE_TIMEOUT);
     };
 
-    var updateOptical = function(value) {
-        console.log('New optical is: ');
-        console.log(value);
-
+    var updateOptical = function (value) {
         var optical = value.value;
-        console.log(optical);
-
-        document.getElementById('optical').textContent = optical.toString() + ' ' + value.unit;
+        document.getElementById('optical').textContent = optical.toString() + ' W/SR';
+        document.getElementById('optical-wrapper').classList.add('animate');
+        setTimeout(function () {
+            document.getElementById('optical-wrapper').classList.remove('animate');
+        }, COLOR_UPDATE_TIMEOUT);
     };
 
-    var updatePressure = function(value) {
-        console.log('New pressure is: ');
-        console.log(value);
-
+    var updatePressure = function (value) {
         var pressure = value.value;
-        console.log(pressure);
-
-        document.getElementById('pressure').textContent = pressure.toString() + ' ' + value.unit;
+        document.getElementById('pressure').textContent = pressure.toString() + ' HPA';
+        document.getElementById('pressure-wrapper').classList.add('animate');
+        setTimeout(function () {
+            document.getElementById('pressure-wrapper').classList.remove('animate');
+        }, COLOR_UPDATE_TIMEOUT);
     };
 
 })();
